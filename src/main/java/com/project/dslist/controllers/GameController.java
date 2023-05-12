2a9bf91c235851a8954d8f9c80b3e6655fc99639
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-05-12 16:47:32 -0300

    GameMinDTO, GameRepository, GameService, GameController

---
+++ src/main/java/com/project/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.project.dslist.controllers;
+
+import com.project.dslist.dto.GameMinDTO;
+import com.project.dslist.entities.Game;
+import com.project.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public ResponseEntity<List<GameMinDTO>> findAll() {
+        List<GameMinDTO> list = gameService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
