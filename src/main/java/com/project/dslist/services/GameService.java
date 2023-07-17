c3893106349d38a15590679aa68b4e36a32021fe
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-07-17 15:28:39 -0300

    Domain model

--- src/main/java/com/project/dslist/services/GameService.java
+++ src/main/java/com/project/dslist/services/GameService.java
@@ -17,6 +17,5 @@ public class GameService  {
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
-
     }
 }
